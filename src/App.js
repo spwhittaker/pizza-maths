5eef8ea05fd9aa695df6eae900bc18405b970dc6
Stephen Whittaker <stephenwhittaker23@gmail.com>
2020-02-24 18:07:44 +0000

    submit 3 options adds pizza to an array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./Components/InputForm";
+import InputForm from "./Components/InputForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pizzarithmetic</h1>
+        <h3>...best thing since sliced bread(pizza)</h3>
       </header>
+      <InputForm />
     </div>
   );
 }
---
+++ src/Components/InputForm.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+
+class InputForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameInput: "",
+      diameterInput: "",
+      priceInput: null,
+      pizzas: []
+    };
+  }
+  handleInput = input => {
+    input.preventDefault();
+    this.state.pizzas.push({
+      name: this.state.nameInput,
+      diameter: this.state.diameterInput,
+      price: this.state.priceInput,
+      quantity: 1
+    });
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleInput}>
+          <div>
+            <div class="pizzaOption">
+              <span class="inputOptions">
+                <p>Name</p>
+                <input
+                  type="text"
+                  required
+                  name="pizzaName"
+                  onChange={event =>
+                    this.setState({ nameInput: event.target.value })
+                  }
+                />
+              </span>
+            </div>
+            <div class="pizzaOption">
+              <span class="inputOptions">
+                <p>Diameter</p>
+                <input
+                  type="number"
+                  required
+                  name="diameter"
+                  onChange={event =>
+                    this.setState({ diameterInput: event.target.value })
+                  }
+                />
+                <input type="radio" id="cm" name="units" value="cm" />
+                <label htmlFor="cm">cm</label>
+                <input type="radio" id="in" name="units" value="in" />
+                <label htmlFor="in">in</label>
+              </span>
+            </div>
+            <div class="pizzaOption">
+              <span class="inputOptions">
+                <p>Price (£)</p>
+                <input
+                  type="number"
+                  required
+                  name="price"
+                  step="0.01"
+                  onChange={event =>
+                    this.setState({ priceInput: event.target.value })
+                  }
+                />
+              </span>
+            </div>
+          </div>
+
+          <button type="submit" label="Submit">
+            I'm hungry!
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default InputForm;
