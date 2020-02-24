ea18e6027428d522ab7a827394df1bbbbfc8e476
Stephen Whittaker <stephenwhittaker23@gmail.com>
2020-02-24 21:27:48 +0000

    refactored calculations and rounding for unit conversion

--- src/Components/PizzaCard.jsx
+++ src/Components/PizzaCard.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import "../styles/PizzaCard.css";
-const PizzaCard = ({ name, diameter, price }) => (
-  <div className="PizzaCard">
-    <h4>Name: {name}</h4>
-    <h4>Diameter (in inches): {diameter}</h4>
-    <h4>Price: £{Number(price).toFixed(2)}</h4>
-    <p>
-      Area: {(Math.PI * Math.pow(diameter / 2, 2)).toFixed(2)} square inches
-    </p>
-    <p>Crust: {(Math.PI * diameter).toFixed(2)} inches</p>
-    <p>
-      Area to crust (bigger means more area compared to crust):{" "}
-      {((Math.PI * Math.pow(diameter / 2, 2)) / (Math.PI * diameter)).toFixed(
-        2
-      )}
-    </p>
-    <p>
-      Price per square inch: £
-      {(Number(price) / (Math.PI * Math.pow(diameter / 2, 2))).toFixed(2)}
-    </p>
-  </div>
-);
+const PizzaCard = ({ name, diameter, price }) => {
+  let roundedDiameter;
+  if (diameter % 1 === 0) {
+    roundedDiameter = diameter;
+  } else {
+    roundedDiameter = Number(diameter).toFixed(1);
+  }
+  const area = (Math.PI * Math.pow(diameter / 2, 2)).toFixed(2);
+  const circumference = (Math.PI * diameter).toFixed(2);
+  return (
+    <div className="PizzaCard">
+      <h4>Name: {name}</h4>
+      <h4>Diameter (in inches): {roundedDiameter}</h4>
+      <h4>Price: £{Number(price).toFixed(2)}</h4>
+      <p>Area: {area} square inches</p>
+      <p>Crust: {circumference} inches</p>
+      <p>
+        Area to crust (bigger means more area compared to crust):{" "}
+        {(area / circumference).toFixed(2)}
+      </p>
+      <p>Price per square inch: £{(Number(price) / area).toFixed(2)}</p>
+    </div>
+  );
+};
 
 export default PizzaCard;
